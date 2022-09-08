81426ecd489d6d9731002a771de647b12058ef7f
Michele Cuff <34314475+Morciara@users.noreply.github.com>
2022-09-07 20:44:13 -0500

    Update SourceServiceImpl.java

--- src/main/java/com/genealogy/researchlog/service/SourceServiceImpl.java
+++ src/main/java/com/genealogy/researchlog/service/SourceServiceImpl.java
@@ -1,6 +1,7 @@
 package com.genealogy.researchlog.service;
 
 import com.genealogy.researchlog.model.Source;
+import com.genealogy.researchlog.repository.SourceRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
@@ -32,7 +33,7 @@ public class SourceServiceImpl implements SourceService {
     }
 
     @Override
-    public boolean update(Source product) {
+    public boolean update(Source source) {
         logger.info("Update source: {}", source);
         return sourceRepository.update(source);
     }
