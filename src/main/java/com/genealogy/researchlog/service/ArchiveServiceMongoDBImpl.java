3ed8bf4ca7e09ca2d3f255c5b2a2f985f70aedc8
Michele Cuff <34314475+Morciara@users.noreply.github.com>
2022-09-15 09:17:56 -0500

    Update ArchiveServiceMongoDBImpl.java

--- src/main/java/com/genealogy/researchlog/service/ArchiveServiceMongoDBImpl.java
+++ src/main/java/com/genealogy/researchlog/service/ArchiveServiceMongoDBImpl.java
@@ -1,6 +1,7 @@
 package com.genealogy.researchlog.service;
 
 import com.genealogy.researchlog.model.Archive;
+import com.genealogy.researchlog.repository.ArchiveRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
